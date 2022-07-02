5ae679b0e60dea90c4730b6185c7ea1be68e7c0d
rivaannn <mrivans2002@gmail.com>
2022-07-02 11:29:53 +0700

    update + fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
         },
         extend: {
             colors: {
-                primary: "#14b8a6",
+                primary: "#38BDF8",
                 secondary: "#64748b",
                 dark: "#0f172a",
             },
